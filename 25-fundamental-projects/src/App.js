fe83d183050583bc450e017e559cb5b6847fb9e6
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-05-22 16:14:27 +0400

    add: new image-slider component

--- 25-fundamental-projects/src/App.js
+++ 25-fundamental-projects/src/App.js
@@ -1,3 +1,4 @@
+import ImageSlider from "./components/image-slider";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -5,7 +6,12 @@ function App() {
   return (
     <div className="App">
       {/* <RandomColor />*/}
-      <StarRating noOfStars={9} />
+      {/*<StarRating noOfStars={9} />*/}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
